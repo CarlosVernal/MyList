c3e4338805acc677f81c4b738f574c167812ae24
CarlosVernal <carlos.vernal.navarrete@gmail.com>
2022-08-31 16:01:48 -0400

    Blueprint de Auth
    se crea la autentificación de usuarios

--- todo/db.py
+++ todo/db.py
@@ -5,6 +5,8 @@ from flask import current_app, g
 from flask.cli import with_appcontext
 from .schema import instructions
 
+#en la variable g se guardara el usuario
+
 def get_db():
     if 'db' not in g:
         g.db = mysql.connector.connect(
