bab95a29a05726b3887e04d77fd0ade6e20e9e01
CarlosVernal <carlos.vernal.navarrete@gmail.com>
2022-09-02 18:27:31 -0400

    correciones, creacion y registro de bp
    agranda atributo password de tamaño 100 a 250

--- todo/auth.py
+++ todo/auth.py
@@ -1,14 +1,14 @@
 import functools
 
 from flask import (
-    blueprints, flash, g, render_template,request, url_for, session, redirect
+    Blueprint, flash, g, render_template,request, url_for, session, redirect
 )
 
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from todo.db import get_db
 
-bp = blueprints('auth', __name__, url_prefix='/auth')
+bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register', methods=['GET','POST'])
 def register():
@@ -18,7 +18,7 @@ def register():
         db, c = get_db()
         error = None
         c.execute(
-            'select id from user where username =%s'
+            'select id from user where username =%s',(username,)
         )
         if not username:
             error = 'Username es requerdio'
@@ -39,27 +39,51 @@ def register():
         flash(error)
     return render_template('auth/register.html')
 
-@bp.route('login', methods=['GET', 'POST'])
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-    db, c = get_db()
-    c.execute(
-        'select * from user where username = %s', (username,) #se usa la coma aunque sea 1 porque es una tupla
-    )
-    user = c.fetchone()
+        db, c = get_db()
+        error = None
+        c.execute(
+            'select * from user where username =%s',(username,)
+        )
+        #se usa la coma luego de username porque es una tupla
+        user = c.fetchone()
 
-    if user is None:
-        error = 'Usuario y/o contraseña inválida'
-    elif not check_password_hash(user['password'], password):
-        error = 'Usuario y/o contraseña inválida'
+        if user is None:
+            error = 'Usuario y/o contraseña inválida'
+        elif not check_password_hash(user['password'], password):
+            error = 'Usuario y/o contraseña inválida'
     
-    if error is None:
-        session.clear()
-        session['user_id'] = user['id']
-        return redirect(url_for('index'))
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
     
-    flash(error)
-
+        flash(error)
     return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        db, c = get_db()
+        c.execute(
+            'select * from user where id = %s', (user_id,)
+        )
+        g.user = c.fetchone()
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view (**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+    
+        return view(**kwargs)
+    
+    return wrapped_view
